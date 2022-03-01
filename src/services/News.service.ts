98063e1b22acd2984144ffc0db514d3185433a31
fladrian <adriandev.fl@gmail.com>
2022-02-28 22:01:38 -0400

    fix: hot fix, topics.ANGULAR enum in News.service

--- src/services/News.service.ts
+++ src/services/News.service.ts
@@ -1,9 +1,10 @@
 import axios from "./Base.service";
 import { INews } from "../interfaces/INews"
 import { AxiosResponse } from "axios";
+import { Topics } from "../constants/Enums";
 
 
-export const GetNews = async(query:string = "angular", page:number = 0): Promise<AxiosResponse<INews>> => {
+export const GetNews = async(query:string = Topics.ANGULAR, page:number = 0): Promise<AxiosResponse<INews>> => {
 	const params = { query, page }
 	return await axios.get("search_by_date",{params})
 }
